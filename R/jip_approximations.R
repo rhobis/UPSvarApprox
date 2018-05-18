7fefd34f6a2f887219c72e93dc58d3827a638d73
Roberto S <rhobis@gmail.com>
2018-05-18 19:29:41 -0400

    minor changes
    added documentation, changed a few names

--- R/jip_approximations.R
+++ R/jip_approximations.R
@@ -14,6 +14,20 @@
 #' vector of first-order inclusion probabilities.
 #'
 #'
+#' @references
+#'
+#' Hartley, H.O.; Rao, J.N.K., 1962. Sampling With Unequal Probability and Without Replacement.
+#' The Annals of Mathematical Statistics 33 (2), 350-374.
+#'
+#' Hájek, J., 1964. Asymptotic Theory of Rejective Sampling with Varying Probabilities from a Finite Population.
+#' The Annals of Mathematical Statistics 35 (4), 1491-1523.
+#'
+#' Tillé, Y., 1996. Some Remarks on Unequal Probability Sampling Designs Without Replacement.
+#' Annals of Economics and Statistics 44, 177-189.
+#'
+#' Brewer, K.R.W.; Donadio, M.E., 2003. The High Entropy Variance of the Horvitz-Thompson Estimator.
+#' Survey Methodology 29 (2), 189-196.
+#'
 #' @export
 
 
@@ -21,7 +35,7 @@
 jip_approx <- function( pik, method ){
 
     ### Check input ---
-    method <- match.arg(method, c( "Hajek", "Hartley-Rao", "IFPF", "Brewer") )
+    method <- match.arg(method, c( "Hajek", "HartleyRao", "Tille", "Brewer18") )
 
     if( !identical( class(pik), "numeric" ) ){
         stop( "pik should be a vector!")
@@ -34,52 +48,83 @@ jip_approx <- function( pik, method ){
     }
 
     ### Call method ---
-    FUN <- switch(method,
-           "Hajek"       = "jip_hajek",
-           "Hartley-Rao" = "jip_hartleyrao",
-           "IPFP"        = "jip_IPFP",
-           "Brewer"      = "jip_brewer"
+    jips <- switch(method,
+           "Hajek"       = jip_Hajek(pik),
+           "HartleyRao"  = jip_HartleyRao(pik),
+           "Tille"       = jip_Tille(pik),
+           "Brewer"      = jip_Brewer(pik, method)
            )
-    jips <- do.call( FUN, list( pik ) )
-
 
     ### Return result ---
     return( jips )
 }
 
-### ADD MORE METHODS ....
 
 
+#' Brewer's joint-inclusion probability approximations
+#'
+#' Approximation of joint inclusion probabilities by one of the estimators
+#' proposed by Brewer and Donadio (2003)
+#'
+#' @inheritParams jip_approx
+#'
+#' @details
+#' \code{"Brewer18"} is the approximation showed in equation (18) of Brewer and Donadio (2003)
+#'
 
-#brw_approx18
-jip_brewer <- function(pik){
-    #Approximation of joint incl. prob with formula 18 from
-    #Brewer & Donadio (2003)
+jip_Brewer <- function(pik, method){
+
+    method <- match.arg(method, c("Brewer18"))
     n <- sum(pik)
 
+    ### Estimate jips ---
     ci <- (n-1) / (n-(2*n-1)/(n-1) * pik + sum(pik**2)/(n-1))
     cc <- outer(ci,ci, '+')
     pp <- outer(pik,pik, '*')
     out <- cc*pp / 2
     diag(out) <- pik
 
+    ### Return result ---
     return(out)
 }
 
-#Function to compute Hajek approximation of joint probabilities
-jip_hajek <- function(pik){
-    #pik = vector of inclusion probabilities in the population
 
-    d <- sum(pik*(1-pik))
-    out <- outer(pik,pik,'*') * (1 - outer(1-pik, 1-pik, '*')/d)
+
+
+#' Hájek's joint-inclusion probability approximation
+#'
+#' Estimate joint-inclusion probabilities using Hájek (1964) equation
+#'
+#'
+#' @inheritParams jip_approx
+#'
+#'
+
+jip_Hajek <- function(pik){
+
+    ### Estimate jips ---
+    d   <- sum(pik*(1-pik))
+    out <- outer(pik,pik,'*') * (1 - outer(1-pik, 1-pik, '*') / d)
     diag(out) <- pik
+
+    ### Return result ---
     return(out)
 }
 
-#Approximation of joint incl. prob by Hartley-Rao (1962)
-#formula with precision of order O(N^{-4})
-jip_hartleyrao <- function(pik){
-    #
+
+
+
+#' Hartley-Rao approximation of joint-inclusion probabilities
+#'
+#' Approximation of joint-inclusion probabilities with precision of order
+#' \eqn{O(N^{-4})} for the random systematic sampling design, by Hartley and Rao (1962).
+#'
+#' @inheritParams jip_approx
+#'
+
+jip_HartleyRao <- function(pik){
+
+    ### Estimate jips ---
     pp <- outer(pik, pik, '*')
     n  <- sum(pik)
     nn <- (n-1)/n
@@ -98,17 +143,26 @@ jip_hartleyrao <- function(pik){
         2*nn/n**3 * pp * sp3
 
     diag(out) <- pik
+
+    ### Return result ---
     return(out)
 }
 
 
-jip_IPFP <- function(pik,eps=1e-06, maxIter=1000){
-    ### approximated second-order inclusion probability by
-    ### means of the Iterative Proportional Fitting Procedure (IPFP)
-    ### [see Tillé, 1996 - Some Remarks on Unequal Probability Sampling ...]
-    if(any(pik>1)) stop("there are inclusion probabilities greater than one")
 
-    ### Input values ---
+#' Tillé's approximation of joint-inclusion probabilities
+#'
+#' Compute the approximation of joint-inclusion probabilities by means of the
+#' Iterative Proportional Fitting Procedure (IPFP) proposed by Tillé (1996)
+#'
+#' @param maxIter a scalar indicating the maximum number of iterations for the
+#' fixed-point procedure
+#' @param eps tolerance value for the convergence of the fixed-point procedure
+#' @inheritParams jip_approx
+#'
+
+jip_Tille <- function(pik,eps=1e-06, maxIter=1000){
+
     n <- sum(pik)
 
     ### Compute b_k values ---
@@ -124,8 +178,11 @@ jip_IPFP <- function(pik,eps=1e-06, maxIter=1000){
         iter <- iter + 1
     }
 
+    ### Estimate jips ---
     out <- outer(b2,b2,'*')
     diag(out) <- pik
+
+    ### Return result ---
     return(out)
 }
 
